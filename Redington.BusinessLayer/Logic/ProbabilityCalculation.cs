047f5c31028acab728729d65f362a7e088d7a26a
BadrulHussain <Badrul.Hussain1@gmail.com>
2021-04-27 02:16:20 +0100

    .

---
+++ Redington.BusinessLayer/Logic/ProbabilityCalculation.cs
@@ -0,0 +1,39 @@
+﻿using Redington.BusinessLayer.Logic.ILogic;
+using Redington.DataAccess.Repository.IRepository;
+using Redington.Models;
+using Redington.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Redington.BusinessLayer.Logic
+{
+    public class ProbabilityCalculation : IProbabilityCalculation
+    {
+        private readonly ICalculationLogRepository _calculationLogRepository;
+        public ProbabilityCalculation(ICalculationLogRepository calculationLogRepository)
+        {
+            this._calculationLogRepository = calculationLogRepository;
+        }
+
+        public float Set(Probability probability)
+        {
+            probability.Date = DateTime.Now.Date;
+
+            if(probability.FunctionType == SD.Function1)
+            {
+                probability.Result = probability.A * probability.B;
+                probability.FunctionType = SD.Function1;
+            }
+            else
+            {
+                probability.Result = probability.A + probability.B - (probability.A * probability.B);
+                probability.FunctionType = SD.Function2;
+            }
+
+            _calculationLogRepository.Create(probability);
+
+            return probability.Result;
+        }
+    }
+}
